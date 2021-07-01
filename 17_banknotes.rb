7bfa7e757bf0672e53e6c589ab570ce9e0d52495
Everton Lopes <everton.lopess@outlook.com>
2021-07-01 11:37:44 -0300

    Refactor/Solution banknotes.

--- 17_banknotes.rb
+++ 17_banknotes.rb
@@ -37,9 +37,10 @@
 #                     1 nota(s) de R$ 2,00
 #                     0 nota(s) de R$ 1,00
 
-max = 1_000_000
+# MEU PROJETO
+MAX = 1_000_000
 min = 0
-banknotes = [100, 50, 20, 10, 5, 2, 1]
+BANK_NOTES = [100, 50, 20, 10, 5, 2, 1].freeze
 count_notes = {
   '100': 0, '50': 0, '20': 0, '10': 0, '5': 0, '2': 0, '1': 0
 }
@@ -49,32 +50,32 @@ puts data_collect
 
 while data_collect > min
   case data_collect
-  when banknotes[0]..max
-    data_collect -= banknotes[0]
+  when BANK_NOTES[0]..MAX
+    data_collect -= BANK_NOTES[0]
     count_notes[:'100'] += 1
 
-  when banknotes[1]..banknotes[0]
-    data_collect -= banknotes[1]
+  when BANK_NOTES[1]..BANK_NOTES[0]
+    data_collect -= BANK_NOTES[1]
     count_notes[:'50'] += 1
 
-  when banknotes[2]..banknotes[1]
-    data_collect -= banknotes[2]
+  when BANK_NOTES[2]..BANK_NOTES[1]
+    data_collect -= BANK_NOTES[2]
     count_notes[:'20'] += 1
 
-  when banknotes[3]..banknotes[2]
-    data_collect -= banknotes[3]
+  when BANK_NOTES[3]..BANK_NOTES[2]
+    data_collect -= BANK_NOTES[3]
     count_notes[:'10'] += 1
 
-  when banknotes[4]..banknotes[3]
-    data_collect -= banknotes[4]
+  when BANK_NOTES[4]..BANK_NOTES[3]
+    data_collect -= BANK_NOTES[4]
     count_notes[:'5'] += 1
 
-  when banknotes[5]..banknotes[4]
-    data_collect -= banknotes[5]
+  when BANK_NOTES[5]..BANK_NOTES[4]
+    data_collect -= BANK_NOTES[5]
     count_notes[:'2'] += 1
 
-  when banknotes[6]..banknotes[5]
-    data_collect -= banknotes[6]
+  when BANK_NOTES[6]..BANK_NOTES[5]
+    data_collect -= BANK_NOTES[6]
     count_notes[:'1'] += 1
 
   else
@@ -86,3 +87,52 @@ end
 count_notes.each do |key, index|
   puts "#{index} nota(s) de R$ #{key},00"
 end
+
+# PROJETO CLAVIUS TALES
+# class Execution
+#   BANK_NOTES = [100, 50, 20, 10, 5, 2, 1].freeze
+#   MAX = 1_000_000
+
+#   def call
+#     request_amount
+#     if invalid_amount?
+#       puts 'Valor inválido.'
+#     else
+#       calculate
+#       show_notes
+#     end
+#   end
+
+#   private
+
+#   attr_reader :amount
+
+#   def request_amount
+#     @amount = gets.to_i
+#   end
+
+#   def invalid_amount?
+#     amount.negative? || amount > MAX
+#   end
+
+#   def calculate
+#     rest = amount
+#     while rest.positive?
+#       note = notes_counting.keys.find { _1 <= rest }
+#       notes_counting[note] += 1
+#       rest -= note
+#     end
+#   end
+
+#   def notes_counting
+#     @notes_counting ||= BANK_NOTES.map { [_1, 0] }.to_h
+#   end
+
+#   def show_notes
+#     notes_counting.each do |note, quantity|
+#       puts "#{quantity} nota(s) de R$#{note},00"
+#     end
+#   end
+# end
+
+# Execution.new.call
